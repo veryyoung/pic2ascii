c08cdca36c0617571e64dd54911a9fc35f051506
veryyoung <codingyoung@gmail.com>
2015-10-19 16:31:50 +0800

    重构成单方法

--- src/me/veryyoung/Pic2Ascii.java
+++ src/me/veryyoung/Pic2Ascii.java
@@ -1,12 +1,16 @@
 package me.veryyoung;
 
-
-import java.awt.image.BufferedImage;
-
 /**
  * Created by veryyoung on 2015/9/28.
  */
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Pic2Ascii {
+
 
     /**
      * Some empirically chosen characters that give good results.
@@ -19,19 +23,54 @@ public class Pic2Ascii {
             System.err.println("请输入正确的图片路径");
             return;
         }
-        BufferedImage image = Tools.getImage(args[0]);
+        System.out.println(transform(args[0]));
 
-        if (null != image) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                for (int x = 0; x < image.getWidth(); x++) {
-                    int index = (int) (Math.random() * defaultCharacters.length);
-                    System.out.print(Tools.isBlack(image.getRGB(x, y)) ? " " : defaultCharacters[index]);
-                    System.out.print(Tools.isBlack(image.getRGB(x, y)) ? " " : defaultCharacters[index]);
-                }
-                System.out.println();
+    }
+
+    public static String transform(String path) {
+        BufferedImage image = getImage(path);
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int index = (int) (Math.random() * defaultCharacters.length);
+                stringBuffer.append(isBlack(image.getRGB(x, y)) ? " " : defaultCharacters[index]);
+                stringBuffer.append(isBlack(image.getRGB(x, y)) ? " " : defaultCharacters[index]);
             }
+            stringBuffer.append("\n\r");
+        }
+        return stringBuffer.toString();
+
+    }
+
+
+    public static boolean isBlack(int pixel) {
+        boolean result = false;
+
+        int r = (pixel >> 16) & 0xff;
+        int g = (pixel >> 8) & 0xff;
+        int b = (pixel) & 0xff;
+
+
+        int tmp = r * r + g * g + b * b;
+        if (tmp > 3 * 128 * 128) {
+            result = true;
         }
 
+        return result;
+    }
+
+    public static BufferedImage getImage(String path) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            System.err.println("未能获取到图片");
+            e.printStackTrace();
+        }
+
+        return image;
     }
 
 }
